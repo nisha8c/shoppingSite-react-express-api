d3d626366c2a2d783b67be6623193afa053c9ae0
Nisha <nisha.chavan@appliedtechnology.se>
2022-11-10 18:44:04 +0100

    image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Products from './Products';
+import manInSuit from './images/depositphotos_11663757-stock-photo-man-in-suit.jpeg';
+
 const Home = () => {
   return (
     <div className='home'>
+      <img src={manInSuit} alt="manInSuit"/>
       Welcome to Home Page..................
       New Arrivals
       Checkout All the Trends...
